ae9293640d9a919fb27496edab142130fd88cd65
aleraiz <alejandroraiz7@gmail.com>
2023-01-17 13:51:23 -0300

    videoMOdal

--- src/components/MovieDetail/MovieDetail.jsx
+++ src/components/MovieDetail/MovieDetail.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { Row } from "react-bootstrap";
+import { Container, Row, Modal, Button } from "react-bootstrap";
 import "./styles.css";
 import { useParams } from "react-router-dom";
+import VideoModal from "./VideoModal";
 
 const MovieDetail = () => {
+  const [modalShow, setModalShow] = useState(false);
   const [movieData, setMovieData] = useState(null);
   const { id } = useParams();
 
@@ -28,27 +30,48 @@ const MovieDetail = () => {
 
   return (
     <>
-      <Row
-        className="movie-detail-row"
+      <Container
+        className="movie-detail-bg d-flex align-items-end"
         style={{
           backgroundImage: movieData
             ? `url(https://image.tmdb.org/t/p/original/${movieData.backdrop_path})`
             : "none",
         }}
-      ></Row>
-      <Row className="mt-5">
-        <div>
-          <iframe
-            width="560"
-            height="315"
-            src="https://www.youtube.com/embed/vGHrJDmepI0"
-            title="YouTube video player"
-            frameborder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-            allowfullscreen
-          ></iframe>
-        </div>
-      </Row>
+      >
+        {/* <Row className="d-flex justify-content-end p-4">
+          <div>
+            <iframe
+              className="movie-trailer"
+              width="360"
+              height="215"
+              src="https://www.youtube.com/embed/vGHrJDmepI0"
+              title="YouTube video player"
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              allowFullScreen
+            ></iframe>
+          </div>
+        </Row> */}
+        <Row className="h-50">
+          <Row className="pb-5">
+            <h5 className="movie-info">
+              {movieData && movieData.original_title}
+            </h5>
+          </Row>
+          <Row className="mb-5">
+            <div className="col-6">
+              <p className="movie-info">{movieData && movieData.overview}</p>
+            </div>
+            <div className="col-6 movie-info d-flex flex-column justify-content-between align-items-center">
+              <div>Vote average: {movieData && movieData.vote_average}</div>
+              <div>Release date: {movieData && movieData.release_date}</div>
+              <div>
+                <VideoModal />
+              </div>
+            </div>
+          </Row>
+        </Row>
+      </Container>
     </>
   );
 };
---
+++ src/components/MovieDetail/VideoModal.jsx
@@ -0,0 +1,70 @@
+import React from "react";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+import "./styles.css";
+import { useEffect, useState } from "react";
+
+const VideoModal = () => {
+  const values = [true, "sm-down", "md-down", "lg-down", "xl-down", "xxl-down"];
+  const [fullscreen, setFullscreen] = useState(true);
+  const [show, setShow] = useState(false);
+
+  function handleShow(breakpoint) {
+    setFullscreen(breakpoint);
+    setShow(true);
+  }
+
+  return (
+    <>
+      <Button className="me-2 mb-2" onClick={() => handleShow()}>
+        Watch Trailer
+      </Button>
+      <Modal show={show} fullscreen={true} onHide={() => setShow(false)}>
+        <Modal.Body className="bg-dark d-flex justify-content-center align-items-center">
+          <iframe
+            className="movie-trailer"
+            src="https://www.youtube.com/embed/vGHrJDmepI0"
+            title="YouTube video player"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowFullScreen
+          ></iframe>
+        </Modal.Body>
+      </Modal>
+    </>
+  );
+};
+
+// const VideoModal = (props) => {
+//   return (
+//     <Modal
+//       {...props}
+//       size="lg"
+//       aria-labelledby="contained-modal-title-vcenter"
+//       centered
+//     >
+//       <Modal.Header closeButton>
+//         {/* <Modal.Title id="contained-modal-title-vcenter">
+//           Modal heading
+//         </Modal.Title> */}
+//       </Modal.Header>
+//       <Modal.Body className="video-modal">
+//         <iframe
+//           className="movie-trailer"
+//           //   width="1000"
+//           //   height="215"
+//           src="https://www.youtube.com/embed/vGHrJDmepI0"
+//           title="YouTube video player"
+//           frameBorder="0"
+//           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+//           allowFullScreen
+//         ></iframe>
+//         {/* <Button onClick={props.onHide}>Close</Button> */}
+//       </Modal.Body>
+//       {/* <Modal.Footer>
+//       </Modal.Footer> */}
+//     </Modal>
+//   );
+// };
+
+export default VideoModal;
